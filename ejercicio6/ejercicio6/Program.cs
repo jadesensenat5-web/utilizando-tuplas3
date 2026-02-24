2c38546b0ecb15676ae39c2ed8e852e027b7bfad
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 17:22:45 -0400

    ejercicio6

---
+++ ejercicio6/ejercicio6/Program.cs
@@ -0,0 +1,6 @@
+﻿// 6. Convertir grados Celsius a Fahrenheit.
+
+Console.Write("Grados Celsius: ");
+double celsius = double.Parse(Console.ReadLine());
+Console.WriteLine("Grados Fahrenheit: " + ToFahrenheit(celsius));
+static double ToFahrenheit(double c) => (c * 9 / 5) + 32;
