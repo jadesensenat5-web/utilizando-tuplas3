1d5fbd5a01b75a39ef67e5a1f174a23b7df75ad5
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:05:14 -0400

    ejercicio10

---
+++ ejercicio10/ejercicio10/Program.cs
@@ -0,0 +1,11 @@
+﻿// 10.Clase Perro
+
+using System;
+
+Perro miPerro = new Perro { raza = "Golden Retriever" };
+miPerro.ImprimirRaza();
+class Perro
+{
+    public string raza;
+    public void ImprimirRaza() => Console.WriteLine($"La raza es: {raza}");
+}
