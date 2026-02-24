c6f819a99b5c7c6296012302a465032943922977
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:10:57 -0400

    ejercicio11

---
+++ ejercicio11/ejercicio11/Program.cs
@@ -0,0 +1,11 @@
+﻿// 11. Clase Coche
+
+using System;
+Coche miCoche = new Coche { color = "Azul" };
+string elColor = miCoche.ObtenerColor();
+Console.WriteLine("El color del coche es: " + elColor);
+class Coche
+{
+    public string color;
+    public string ObtenerColor() => color;
+}
