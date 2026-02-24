a605107099d163114ff43db7da1979308ca34e8a
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 17:26:13 -0400

    ejercicio7

---
+++ ejercicio7/ejercicio7/Program.cs
@@ -0,0 +1,8 @@
+﻿// 7. Verificar si un número está en una lista
+
+using System.Linq;
+int[] numeros = { 10, 20, 30, 40, 50 }; // Tu lista de ejemplo
+Console.Write("Busca un número en la lista: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(EstaEnLista(numeros, n) ? "Sí está en la lista" : "No se encontró");
+static bool EstaEnLista(int[] lista, int num) => lista.Contains(num);
