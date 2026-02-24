376644b3f488a37360edfda424f5089be2254207
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 17:37:59 -0400

    ejercicio8

---
+++ ejercicio8/ejercicio8/Program.cs
@@ -0,0 +1,7 @@
+﻿// 8. Calcular la longitud de una cadena
+
+Console.Write("Escribe una palabra o frase: ");
+string entrada = Console.ReadLine();
+
+Console.WriteLine("La longitud es: " + ObtenerLargo(entrada));
+static int ObtenerLargo(string s) => s.Length;
