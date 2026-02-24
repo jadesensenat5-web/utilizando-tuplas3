c157faeb2fb1eb1e5ad0eab1929e7af0a8670af5
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:34:18 -0400

    ejercicio13

---
+++ ejercicio13/ejercicio13/Program.cs
@@ -0,0 +1,12 @@
+﻿// 13. tupla con los números del 1 al 5 e imprime el tercer 
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        var numeros = (1, 2, 3, 4, 5);
+        Console.WriteLine("El tercer elemento es: " + numeros.Item3);
+        Console.ReadKey();
+    }
+}
