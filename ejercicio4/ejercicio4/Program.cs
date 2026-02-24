73045ad6d6f357ff8e4d5ff25e05ef5cc608e445
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 16:50:49 -0400

    ejercicio4

---
+++ ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,9 @@
+﻿// 4. Encontrar el mayor de dos numeros
+
+using System;
+Console.Write("Primer número: ");
+double n1 = double.Parse(Console.ReadLine());
+Console.Write("Segundo número: ");
+double n2 = double.Parse(Console.ReadLine());
+Console.WriteLine("El mayor es: " + EncontrarMayor(n1, n2));
+static double EncontrarMayor(double a, double b) => a > b ? a : b;
