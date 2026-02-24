b7096efeeae85abe7a58d3eff5b52555601512b3
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:39:13 -0400

    ejercicio14

---
+++ ejercicio14/ejercicio14/Program.cs
@@ -0,0 +1,6 @@
+﻿// 14. Tuplas de las frutas y verduras}
+
+var frutas = ("Manzana", "Pera", "Uva");
+var verduras = ("Tomate", "Lechuga", "Pepino");
+var todo = (frutas.Item1, frutas.Item2, frutas.Item3, verduras.Item1, verduras.Item2, verduras.Item3);
+Console.WriteLine("Tupla completa: " + todo);
