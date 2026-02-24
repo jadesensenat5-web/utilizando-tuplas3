8113201d287d7b7b18be705103d1435bea54c0f2
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:59:45 -0400

    ejercicio18

---
+++ ejercicio18/ejercicio18/Program.cs
@@ -0,0 +1,8 @@
+﻿// 18. Contar repetidos en una tupla
+
+using System.Linq;
+var t10 = (1, 2, 2, 3, 2, 4);
+int[] arregloTemp = { t10.Item1, t10.Item2, t10.Item3, t10.Item4, t10.Item5, t10.Item6 };
+int cantidad = arregloTemp.Count(n => n == 2);
+
+Console.WriteLine("El número 2 aparece: " + cantidad + " veces");
