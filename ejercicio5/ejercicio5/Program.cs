5299f43fe198f94bb00abf24f0b7f8a44494d6a6
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 16:59:38 -0400

    ejercicio5

---
+++ ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,7 @@
+﻿// 5. Programa que cuente el numero de vocales en una cadena
+
+using System.Linq;
+Console.Write("Escribe una frase: ");
+string texto = Console.ReadLine();
+Console.WriteLine("Total de vocales: " + ContarVocales(texto));
+static int ContarVocales(string t) => t.Count(c => "aeiouAEIOU".Contains(c));
