bfe00e185bbec4cf1424bb52d501e6a01496ec0d
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:55:45 -0400

    ejercicio17

---
+++ ejercicio 17/ejercicio 17/Program.cs
@@ -0,0 +1,13 @@
+﻿// 17. Tupla de ciudades a Lista
+
+using System.Collections.Generic;
+var ciudadesTupla = ("SD", "Santiago", "Moca", "La Vega", "Puerto Plata");
+var listaCiudades = new List<string> {
+    ciudadesTupla.Item1,
+    ciudadesTupla.Item2,
+    ciudadesTupla.Item3,
+    ciudadesTupla.Item4,
+    ciudadesTupla.Item5
+};
+
+Console.WriteLine("Primer ciudad de la lista: " + listaCiudades[0]);
