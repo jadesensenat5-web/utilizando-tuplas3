a02015d724cbc8f2c2490f728ca3b7f21c321c64
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 16:13:58 -0400

    ejercicio3

---
+++ ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,6 @@
+﻿// // 3. numero par
+
+Console.Write("Número: "); int n = int.Parse(Console.ReadLine());
+Console.WriteLine(EsPar(n) ? "Es Par" : "Es Impar");
+
+static bool EsPar(int num) => num % 2 == 0;
