8951a14ee91c05414a53ce6daed9cf75a7dc088a
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 17:56:55 -0400

    ejercicio9

---
+++ ejercicio9/ejercicio9/Program.cs
@@ -0,0 +1,10 @@
+﻿// 9. Crear una clase persona.
+
+using System;
+Persona p = new Persona { nombre = "Jades" };
+p.Saludar();
+class Persona
+{
+    public string nombre;
+    public void Saludar() => Console.WriteLine($"Hola, soy {nombre}");
+}
