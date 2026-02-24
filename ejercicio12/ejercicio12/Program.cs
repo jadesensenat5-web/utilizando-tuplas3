73e0d52eb51fdcf98b78436931e3fe45dc851b17
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 18:22:08 -0400

    ejercicio12

---
+++ ejercicio12/ejercicio12/Program.cs
@@ -0,0 +1,9 @@
+﻿// 12. Clase Libro
+using System;
+Libro miLibro = new Libro { titulo = "Cien años de soledad" };
+miLibro.ImprimirTitulo();
+class Libro
+{
+    public string titulo;
+    public void ImprimirTitulo() => Console.WriteLine("Título del libro: " + titulo);
+}
