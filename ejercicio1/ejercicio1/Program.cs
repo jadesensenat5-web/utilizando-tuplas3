4b9a453f7c8f1782b6bca7cdb97e53d338ca4bd9
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-24 12:11:26 -0400

    ejercicio1
    ejercicio1

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,6 @@
+﻿// 1. suma de dos numeros
+Console.Write("Num 1: "); double a = double.Parse(Console.ReadLine());
+Console.Write("Num 2: "); double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Suma: " + Sumar(a, b));
+
+static double Sumar(double n1, double n2) => n1 + n2;
